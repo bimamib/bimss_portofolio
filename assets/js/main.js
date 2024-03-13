8d79ff3d85e54255ce05cddad54e7c2f72104ecb
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-03-14 02:21:24 +0700

    feat: membuat function mixitup, modal work

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,70 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick) {
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalClose.forEach((mc) => {
+  mc.addEventListener('click', () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove('active-modal')
+    })
+  })
+})
+
+/*=============== WORK MODAL ===============*/
+const modalViewsWork = document.querySelectorAll('.work__modal'),
+  modalBtnsWork = document.querySelectorAll('.work__button'),
+  modalCloseWork = document.querySelectorAll('.work__modal-close')
+
+let modal_work = function(modal_workCLick) {
+  modalViewsWork[modal_workCLick].classList.add('active-modal-work')
+}
+
+modalBtnsWork.forEach((mbw, i) => {
+  mbw.addEventListener('click', () => {
+    modal_work(i)
+  })
+})
+
+modalCloseWork.forEach((mcw) => {
+  mcw.addEventListener('click', () => {
+    modalViewsWork.forEach((mvw) => {
+      mvw.classList.remove('active-modal-work')
+    })
+  })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+var mixerPortofolio = mixitup('.work__container', {
+  selectors: {
+      target: '.work__card'
+  },
+  animation: {
+      duration: 300
+  }
+});
 
 /* Link active work */ 
+const linkWork = document.querySelectorAll('.work__item')
 
+function activeWork() {
+  linkWork.forEach(l => l.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
